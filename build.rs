f2649432d4731efd2458d1a35519a01217b30be7
Eduardo Flores <edfloreshz@gmail.com>
2026-08-14 13:13:00 -0700

    add: profiles protocol

--- build.rs
+++ build.rs
@@ -1,10 +1,18 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=proto/winebridge.proto");
     tonic_prost_build::configure()
+        .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
         .type_attribute(
             ".",
             "#[derive(serde::Serialize, serde::Deserialize)] #[serde(rename_all = \"kebab-case\")]",
         )
-        .compile_protos(&["proto/winebridge.proto"], &["proto/"])?;
+        .compile_protos(
+            &[
+                "proto/winebridge.proto",
+                "proto/bottles/profiles/v1/profiles.proto",
+                "proto/bottles/common/v1/common.proto",
+            ],
+            &["proto/"],
+        )?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
 pub mod winebridge {
     tonic::include_proto!("winebridge");
 }
+
+pub mod bottles {
+    pub mod common {
+        pub mod v1 {
+            tonic::include_proto!("bottles.common.v1");
+        }
+    }
+    pub mod profiles {
+        pub mod v1 {
+            tonic::include_proto!("bottles.profiles.v1");
+        }
+    }
+}
