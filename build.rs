54fb4a952ffd92a7fdbb89bc19d2cd77b35b05ea
Eduardo Flores <edfloreshz@gmail.com>
2026-08-14 23:27:22 -0700

    add: profiles, library and store protocols

--- build.rs
+++ build.rs
@@ -1,15 +1,21 @@
+use std::{env, path::PathBuf};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=proto/winebridge.proto");
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
     tonic_prost_build::configure()
         .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
         .type_attribute(
             ".",
             "#[derive(serde::Serialize, serde::Deserialize)] #[serde(rename_all = \"kebab-case\")]",
         )
+        .file_descriptor_set_path(out_dir.join("bottles_descriptor.bin"))
         .compile_protos(
             &[
                 "proto/winebridge.proto",
                 "proto/bottles/profiles/v1/profiles.proto",
+                "proto/bottles/store/v1/store.proto",
+                "proto/bottles/library/v1/library.proto",
                 "proto/bottles/common/v1/common.proto",
             ],
             &["proto/"],
--- src/lib.rs
+++ src/lib.rs
@@ -13,4 +13,16 @@ pub mod bottles {
             tonic::include_proto!("bottles.profiles.v1");
         }
     }
+    pub mod store {
+        pub mod v1 {
+            tonic::include_proto!("bottles.store.v1");
+        }
+    }
+    pub mod library {
+        pub mod v1 {
+            tonic::include_proto!("bottles.library.v1");
+        }
+    }
 }
+
+pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("bottles_descriptor");
